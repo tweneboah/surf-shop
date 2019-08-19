11b82bdb222bd167ee5c98b5ec02d0375b6419b0
Emmanuel Tweneboah <twentekghana@gmail.com>
2019-08-19 14:50:38 +0800

    Passport authentication setup

--- surf-shop-v6/app.js
+++ surf-shop-v6/app.js
@@ -34,6 +34,9 @@ app.use(session({
  
 }))
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
