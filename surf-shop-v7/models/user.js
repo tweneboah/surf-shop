c32a12489f1d25013f4c3cd59947bdcefd3e4680
Emmanuel Tweneboah <twentekghana@gmail.com>
2019-08-19 12:19:09 +0800

    Register user with passport route created

---
+++ surf-shop-v7/models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const PassportLocalMongoose = require('passport-local-mongoose');
+
+const userSchema = new mongoose.Schema({
+   
+   password: String,//Passport will create this for us automatically
+   username: String, //Passport will create this for us automatically
+   
+   email: String,
+   image: String,
+   posts: [{
+         type: mongoose.Schema.Types.ObjectId,
+         ref: 'Post'
+       }]
+});
+
+userSchema.plugin(PassportLocalMongoose)
+
+module.exports = mongoose.model('User', userSchema)
