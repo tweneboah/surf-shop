dce0fa9d2824033d02a61168022a4be505a02996
Emmanuel Tweneboah <twentekghana@gmail.com>
2019-08-19 14:36:02 +0800

    Post Register Route refactered

---
+++ surf-shop-v10/app.js
@@ -0,0 +1,83 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const mongoose = require('mongoose');
+//passport
+const passport = require('passport');
+const passportLocal = require('passport-local');
+//Models
+const User = require('./models/user')
+// routes
+const indexRouter = require('./routes/index');
+const postRouter = require('./routes/posts');
+
+
+//DB CONNECTION
+mongoose.connect('mongodb://localhost/Surf-Shop', {
+ useNewUrlParser: true,
+ useCreateIndex: true,
+ useFindAndModify: false
+})
+.then(() => console.log("DB Connected successfully"));
+
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+//==========================
+// CONFIGURE PASSPORT AND SESSIONS
+// Without express sessions, passport will not work
+//==========================
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+ 
+}))
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
+//MIDDLEWARE
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+//MOUNT ROUTES
+
+app.use('/', indexRouter);
+app.use('/posts', postRouter)
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ surf-shop-v10/controllers/index.js
@@ -0,0 +1,30 @@
+const User = require('../models/user');
+const express = require('express');
+const router = express.Router()
+
+
+module.exports = {
+  
+ postRegister(req, res, next) {
+   
+      //We are passing other fields to register function
+
+      const newUser = {
+        email: req.body.email,
+        image: req.body.image,
+        username: req.body.username
+      }
+       User.register(new User(newUser), req.body.password, (err) => {
+         if (err) {
+           console.log('error while user register!', err);
+           return next(err);
+         }
+     
+         console.log('user registered!');
+     
+         res.redirect('/');
+       });
+     
+  }
+
+}
