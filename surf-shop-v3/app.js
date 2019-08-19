710a4a05e6c734ce2b2ae77b20f9093caabdf8c1
Emmanuel Tweneboah <twentekghana@gmail.com>
2019-08-19 09:57:37 +0800

    Reviews for s post route created

--- surf-shop-v3/app.js
+++ surf-shop-v3/app.js
@@ -7,8 +7,7 @@ const logger = require('morgan');
 //Requiring routes
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const postRouter = require('./routes/posts');
-const reviewsRouter = require('./routes/reviews');
+const postRouter = require('./routes/posts')
 
 const app = express();
 
@@ -29,8 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/posts', postRouter);
-app.use('/pots/:id/reviews', reviewsRouter);
+app.use('/posts', postRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
